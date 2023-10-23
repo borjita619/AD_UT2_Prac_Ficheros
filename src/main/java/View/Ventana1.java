a7818664e035ee93e628f81f81ae7362e36ba627
borjagl <borjagl.alumno@iesiniesta.es>
2023-10-23 10:22:47 +0200

    Interfaz creada

--- src/main/java/View/Ventana1.java
+++ src/main/java/View/Ventana1.java
@@ -17,4 +17,23 @@ public class Ventana1 extends JFrame {
     private JButton button2;
     private JButton button3;
     private JPanel panelPadre;
+    private JButton analizarButton;
+    private JTable table1;
+    private JTable table2;
+    private JTextArea textArea1;
+    private JButton borrarSeleccionadoButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton añadirDatosButton;
+    private JButton actualizarSeleccionadoButton;
+    private JButton exportarAFicheroJSONButton;
+    private JButton exportarAFicheroXMLButton1;
+    private JButton exportarAFicheroXMLButton;
+    private JButton importarDeFicheroJSONButton;
+    private JButton convertirXMLAJSONButton;
+
+    public static void main(String[] args) {
+        new Ventana1();
+    }
 }
