f3886677cd174c528b55959f4c0766b17c06ceba
borjagl <borjagl.alumno@iesiniesta.es>
2023-10-27 10:34:19 +0200

    Cambios en los botones

--- src/main/java/Model/Fichero.java
+++ src/main/java/Model/Fichero.java
@@ -1,4 +1,83 @@
 package Model;
 
-public class Fichero {
+import java.io.Serializable;
+
+public class Fichero implements Serializable {
+    int id;
+    String nombre;
+    String fechaHoraModif;
+    long tamanioFich;
+    String sha1;
+    String md5;
+    public Fichero() {
+    }
+
+    public Fichero(int id, String nombre, String fechaHoraModif, long tamanioFich, String sha1, String md5) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaHoraModif = fechaHoraModif;
+        this.tamanioFich = tamanioFich;
+        this.sha1 = sha1;
+        this.md5 = md5;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaHoraModif() {
+        return fechaHoraModif;
+    }
+
+    public void setFechaHoraModif(String fechaHoraModif) {
+        this.fechaHoraModif = fechaHoraModif;
+    }
+
+    public long getTamanioFich() {
+        return tamanioFich;
+    }
+
+    public void setTamanioFich(long tamanioFich) {
+        this.tamanioFich = tamanioFich;
+    }
+
+    public String getSha1() {
+        return sha1;
+    }
+
+    public void setSha1(String sha1) {
+        this.sha1 = sha1;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+
+    @Override
+    public String toString() {
+        return "Fichero{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", fechaHoraModif='" + fechaHoraModif + '\'' +
+                ", tamanioFich=" + tamanioFich +
+                ", sha1='" + sha1 + '\'' +
+                ", md5='" + md5 + '\'' +
+                '}';
+    }
 }
--- src/main/java/View/Ventana1.java
+++ src/main/java/View/Ventana1.java
@@ -2,20 +2,46 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
 
 public class Ventana1 extends JFrame {
+    final String rutaAAnalizar="C:\\_Pruebas_Programacion";
     public Ventana1() {
         setTitle("GESTOR GIMNASIO");
         setContentPane(panelPadre);
-        setMaximumSize(new Dimension(1200,700));
+        setMinimumSize(new Dimension(1200,700));
         setLocationRelativeTo(getParent());
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        textFieldRutaActual.setText(rutaAAnalizar);
+        examinarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+                int result = fileChooser.showOpenDialog(null);
+
+                if (result != JFileChooser.CANCEL_OPTION) {
+
+                    File fileName = fileChooser.getSelectedFile();
+
+                    if ((fileName == null) || (fileName.getName().equals(""))) {
+                        textFieldRutaActual.setText("...");
+                    } else {
+                        textFieldRutaActual.setText(fileName.getAbsolutePath());
+                    }
+                }
+            }
+        });
     }
 
-    private JTextField textField1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JTextField textFieldRutaActual;
+    private JButton examinarButton;
+    private JButton nuevoButton;
+    private JButton renombrarSeleccionadoButton;
     private JPanel panelPadre;
     private JButton analizarButton;
     private JTable table1;
@@ -34,6 +60,8 @@ public class Ventana1 extends JFrame {
     private JButton convertirXMLAJSONButton;
 
     public static void main(String[] args) {
-        new Ventana1();
+        new Ventana1().setVisible(true);
+
+
     }
 }
